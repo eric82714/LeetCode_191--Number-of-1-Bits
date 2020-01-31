62605cc061aa20448c5ab26fd849d05a0948ed01
eric82714 <0958820714ab@gmail.com>
2020-01-31 23:49:51 +0800

    Create number_of_1_bits.c

---
+++ C/number_of_1_bits.c
@@ -0,0 +1,10 @@
+int hammingWeight(uint32_t n) {
+    int counter = 0;
+        
+    while(n > 0) {
+        if(n % 2 == 1) counter += 1;
+        n = n / 2;
+    }
+            
+    return counter;
+}
